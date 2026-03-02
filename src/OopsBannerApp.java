ee35f6f50c06e67e35a88fea519a69b1cba4ab17
gr7347-byte <gr7347@srmist.edu.in>
2026-03-02 13:06:13 +0530

    Uc5

---
+++ src/OopsBannerApp.java
@@ -0,0 +1,17 @@
+public class OopsBannerApp {
+    public static void main(String[] args) {
+        String[] line = {
+                (String.join(" ", "   ***   ", "   ***   ", "******   ", "  ***** ")),
+                (String.join(" ", " **   ** ", " **   ** ", "**    ** ", "**       ")),
+                (String.join(" ", "**     **", "**     **", "**    ** ", " **      ")),
+                (String.join(" ", "**     **", "**     **", "******   ", "   ***   ")),
+                (String.join(" ", "**     **", "**     **", "**       ", "      ** ")),
+                (String.join(" ", " **   ** ", " **   ** ", "**       ", "**     **")),
+                (String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  ")),
+        };
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(line[i]);
+        }
+    }
+}
